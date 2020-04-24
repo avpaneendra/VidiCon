e391d2fa48b295919f884db9a105e8f895041103
Salman - MacOS <epbd.saif@gmail.com>
2020-04-25 01:16:26 +0600

    mp fix.

--- src/meeting.js
+++ src/meeting.js
@@ -38,6 +38,6 @@ document.getElementById("myButton").addEventListener("click", function() {
     
     }
 
-    mixpanel.track("Meeting Initiated", {"page": "meeting.html", "room":options['roomName']});
+    mixpanel.track("Meeting Initiated", {"page": "meeting.html", "room":options['roomName'], "user":localStorage.thisUserName});
 
 })
